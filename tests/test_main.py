9e6562d7e48a2c10ae13c73871289460e516772f
unknown <armeldrey@gmail.com>
2022-07-01 22:29:07 +0000

    update Jenkinsfile

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,5 +15,5 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Heello World"}
+    assert response.json() == {"msg": "Hello World"}
 
